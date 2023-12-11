2cff98400fefd7c05b052f614ed3378c61ec66d7
Zapi96 <88500729+Zapi96@users.noreply.github.com>
2023-12-11 17:42:02 +0100

    Update streamlit_app.py
    added some exceptions and if statements

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,16 +26,17 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice api response
-streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-
-# Normalize previous json response using pandas
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Showing resulting dataframe
-streamlit.dataframe(fruityvice_normalized)
+try:
+  streamlit.header("Fruityvice Fruit Advice!")
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error('Please select a fruit to get information')
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
